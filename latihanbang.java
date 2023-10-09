1bfcb40ff2327981617f226c0465985ea744e697
abhixyz1 <irfanhendra90@gmail.com>
2023-10-09 12:24:15 +0700

    njajal...

---
+++ latihanbang.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class latihanbang{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // input //  
+        String nama, jenis_kereta, rute;
+        int harga;
+
+        System.out.println("=== SELAMAT DATANG DI SISTEM PEMESANAN TIKET KERETA API ===");
+
+        System.out.println("Masukkan nama:");
+        nama = input.next();
+
+        System.out.println("Masukkan Kelas Kereta Api (Ekonomi/Eksekutif/Bisnis) : ");
+        jenis_kereta = input.next();
+
+        System.out.println("Masukkan Rute Tujuan:");
+        rute = input.next();
+
+        System.out.println("Masukkan Tanggal Keberangkatan (dd/mm/yyyy) :");
+        String TglBerangkat = input.next();
+
+
+
+
+
+    }
+}
